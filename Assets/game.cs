50261f9d5c0717eefa2ccadb0a60a5f8c0b3a9f8
mikgorn <kirill_777_07@inbox.ru>
2018-09-07 23:17:50 +0900

    added basic ui

---
+++ Assets/game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class game : MonoBehaviour {
+
+    public ulong units = 0;
+    public Text units_text;
+
+    public Button virus_click;
+
+	// Use this for initialization
+	void Start () {
+        virus_click.onClick.AddListener(virus_clicked);
+	}
+
+    void virus_clicked()
+    {
+        units += 1;
+    }
+	// Update is called once per frame
+	void Update () {
+        units_text.text = "Units: " + units.ToString();
+
+
+	}
+}
